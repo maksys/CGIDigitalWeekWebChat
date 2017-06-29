29b5c309e6e080c82d1f05f27d981f2ae4434b59
frederic macaigne <frederic.macaigne@maksys.fr>
2017-06-29 09:20:05 +0200

    Add project files.

---
+++ CGIDigitalWeekWebChat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CGIDigitalWeekWebChat.Startup))]
+namespace CGIDigitalWeekWebChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
