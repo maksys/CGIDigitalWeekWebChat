a8e843ee2dff0f2473ee5b6f6aca2e982285a812
frederic macaigne <frederic.macaigne@maksys.fr>
2017-07-13 10:06:39 +0200

    Add Web API for number of person for a stand ID

---
+++ CGIDigitalWeekWebChat/Controllers/CrowdController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CGIDigitalWeekWebChat.Models;
+
+namespace CGIDigitalWeekWebChat.Controllers
+{
+    public class CrowdController : ApiController
+    {
+        [Route("Crowd/GetPersons/{standId}")]
+        public IHttpActionResult GetStandPersons(string standId)
+        {
+            var persons = CrowdModels.GetPersons(standId);
+            if (!persons.HasValue)
+            {
+                return NotFound();
+            }
+            return Ok(persons.Value);
+        }
+
+
+    }
+}
---
+++ CGIDigitalWeekWebChat/Models/CrowdModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace CGIDigitalWeekWebChat.Models
+{
+    public static class CrowdModels
+    {
+        private static string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;AccountName=nestorhubstorage;AccountKey=s9QL3M9moilOSR4xZ8qJVR1ngLaOO7n0pg/Vzu8zbEKODTWpGkYmXsZf1ZC0fERhuP2JDetI17w59hWjgIj+CQ==;EndpointSuffix=core.windows.net";
+        public static int? GetPersons(string standId)
+        {
+            var storageAccount = CloudStorageAccount.Parse(StorageAccountConnectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable iotTable = tableClient.GetTableReference("deviceData");
+            TableQuery query = new TableQuery();
+            //query.Select(new List<string>() {"message"});
+            query.SelectColumns = new List<string>() { "deviceId", "persons" };
+            query.Where(TableQuery.GenerateFilterCondition("deviceId", QueryComparisons.Equal, standId));
+            var results = iotTable.ExecuteQuery(query);
+            var result =  from elements in results
+                           where elements.Timestamp == results.Max(c => c.Timestamp)
+                           select new int?( elements.Properties["persons"].Int32Value.Value );
+            return result.FirstOrDefault();
+        }
+
+    }
+}
